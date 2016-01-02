262cd59ff8b920a468bf93a3305d411ed43c2f47
Stephen.Spence86 <stephen.spence86@gmail.com>
2016-01-02 23:21:38 +0000

    Added Header

--- pinary-lights.py
+++ pinary-lights.py
@@ -1,3 +1,13 @@
+"""
+
+Pinary-Lights
+
+Python Script to evaulate decimal number to binary and light up corresponding
+LEDs controlled by a Raspberry Pi 2 GPIO interface.
+
+This script uses broadcom numbering for the GPIO pins and the gpiozero library. 
+"""
+
 from gpiozero import LED
 from signal import pause
 
