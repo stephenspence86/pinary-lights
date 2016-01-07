21b7c5041ae4bc5ac9c6a784778e3da1b6dbfcfc
Stephen Spence <stephen.spence86@gmail.com>
2016-01-07 15:38:53 +0000

    Code 1
    Code 1

--- pinary-lights.py
+++ pinary-lights.py
@@ -5,12 +5,21 @@ Pinary-Lights
 Python Script to evaulate decimal number to binary and light up corresponding
 LEDs controlled by a Raspberry Pi 2 GPIO interface.
 
-This script uses broadcom numbering for the GPIO pins and the gpiozero library. 
+This script uses broadcom numbering for the GPIO pins and the gpiozero library.
 """
 
 from gpiozero import LED
 from signal import pause
 
 
-x = input("Enter a Number between 1 and 255: ")
+DecNumber = input("Enter a Number between 1 and 255: ")
 
+    if DecNumber < 1 or > 255
+        print ("Pick a Number between 1 and 255: ")
+        DecNumber = input
+        else # Modulo division for DecNumber to BinNumber
+
+    if bit1 == 1
+        LED(6).on
+        else
+        LED(6).off
