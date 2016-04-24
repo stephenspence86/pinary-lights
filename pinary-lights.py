43cf1d2632a33222c2ddc76a3022551a151558c4
Stephen Spence <stephen.spence86@gmail.com>
2016-04-24 23:49:07 +0100

    Final
    Working Code

--- pinary-lights.py
+++ pinary-lights.py
@@ -9,17 +9,34 @@ This script uses broadcom numbering for the GPIO pins and the gpiozero library.
 """
 
 from gpiozero import LED
-from signal import pause
+import time
 
+Bit1 = LED(17)
+Bit2 = LED(27)
+Bit3 = LED(22)
+Bit4 = LED(5)
+Bit5 = LED(6)
+Bit6 = LED(13)
+Bit7 = LED(19)
+Bit8 = LED(26)
 
-DecNumber = input("Enter a Number between 1 and 255: ")
+Bits = [Bit1, Bit2, Bit3, Bit4, Bit5, Bit6, Bit7, Bit8]
 
-    if DecNumber < 1 or > 255
-        print ("Pick a Number between 1 and 255: ")
-        DecNumber = input
-        else # Modulo division for DecNumber to BinNumber
+for n in range (0, 8):
+    Bits[n].off()
 
-    if bit1 == 1
-        LED(6).on
-        else
-        LED(6).off
+x = int(input("Enter a Number between 1 and 255: "))
+
+while x < 1 or x > 255:
+
+    x = int(input ("Enter a Number between 1 and 255:"))
+
+for n in range (0, 8):
+    rem = x%2
+    if rem == 1:
+        Bits[n].on()
+    x = x // 2
+
+time.sleep(5)
+for n in range (0, 8):
+    Bits[n].off()
